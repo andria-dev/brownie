1cfa98ee477cb9f74202f28c44a8cff5fcbf8bfd
Christopher Brown <chbphone55@gmail.com>
2019-05-02 01:12:43 -0600

    fixed it up

--- pages/post.js
+++ pages/post.js
@@ -1,3 +1,9 @@
-function Post({ query }) {
-  return <p className="text-blue">{query.id}</p>
+import { withRouter } from 'next/router'
+
+import '../global.css'
+
+function Post({ router }) {
+  return <p className="text-blue">{router.query.id}</p>
 }
+
+export default withRouter(Post)
