3db9f2ef4b9fd35f934519833307aa0afa4724b4
Christopher Brown <chbphone55@gmail.com>
2019-05-01 21:12:20 -0600

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
