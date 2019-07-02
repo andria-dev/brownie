c41ccb67d9db84dbe85dbefee87c09f60bbc5f2b
Christopher Brown <chbphone55@gmail.com>
2019-07-01 23:47:44 -0600

    delete erything

--- pages/post.js
+++
@@ -1,9 +0,0 @@
-import { withRouter } from 'next/router'
-
-import '../global.css'
-
-function Post({ router }) {
-  return <p className="text-blue">{router.query.id}</p>
-}
-
-export default withRouter(Post)
