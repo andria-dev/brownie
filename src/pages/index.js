419b3d87f468f434c19cb952710cbcb3558e6476
Christopher Brown <chbphone55@gmail.com>
2019-07-03 23:03:21 -0600

    import scale

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link, graphql } from 'gatsby'
 import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { rhythm } from '../utils/typography'
+import { rhythm, scale } from '../utils/typography'
 
 class BlogIndex extends React.Component {
   render() {
