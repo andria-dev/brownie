d5cd9bd3d64239f0b43b6e2bad83a8627033c0e9
Christopher Brown <chbphone55@gmail.com>
2019-12-03 12:34:02 -0600

    fix list item a11y

---
+++ src/components/BlogPostComponents/ListItemParagraph.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListItemParagraph({ children }) {
+  return (
+    <li>
+      <p>{children}</p>
+    </li>
+  )
+}
+
+export default ListItemParagraph
--- src/components/BlogPostComponents/index.js
+++ src/components/BlogPostComponents/index.js
@@ -1,10 +1,12 @@
 import rehypeReact from 'rehype-react'
 import { createElement } from 'react'
 import AccessibleInlineCode from './AccessibleInlineCode'
+import ListItemParagraph from './ListItemParagraph'
 
 export const renderAST = new rehypeReact({
   createElement: createElement,
   components: {
-    code: AccessibleInlineCode
+    code: AccessibleInlineCode,
+    li: ListItemParagraph
   }
 }).Compiler
