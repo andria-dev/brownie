4b430aa013fd1b3ea2b82f890e8d516ccf8636fd
ChrisBrownie55 <chbphone55@gmail.com>
2020-05-09 15:22:52 -0500

    Fix style and formatting

--- src/components/layout.js
+++ src/components/layout.js
@@ -69,7 +69,7 @@ class Layout extends React.Component {
           marginLeft: `auto`,
           marginRight: `auto`,
           maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)} ${rhythm(40)}`,
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)} ${rhythm(4)}`,
         }}
       >
         <header>{header}</header>
--- src/templates/blog-post/index.js
+++ src/templates/blog-post/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
 import Bio from '../../components/bio'
 import Layout from '../../components/layout'
@@ -10,96 +10,92 @@ import { rhythm, scale } from '../../utils/typography'
 import 'prism-themes/themes/prism-base16-ateliersulphurpool.light.css'
 import './styles.css'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+export default function BlogPostTemplate(props) {
+  const post = props.data.markdownRemark
+  const siteTitle = props.data.site.siteMetadata.title
+  const { previous, next } = props.pageContext
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        {!post.frontmatter.published ? (
-          <p
-            style={{
-              ...scale(-1 / 5),
-              marginLeft: rhythm(1 / 8),
-              marginTop: rhythm(1),
-              marginBottom: 0,
-              opacity: 0.8,
-              fontWeight: 'bold',
-              fontFamily: 'sans-serif',
-            }}
-          >
-            DRAFT
-          </p>
-        ) : null}
-        <h1
-          style={{
-            marginTop: post.frontmatter.published ? rhythm(1) : 0,
-            marginBottom: 0,
-          }}
-        >
-          {post.frontmatter.title}
-        </h1>
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      {!post.frontmatter.published ? (
         <p
           style={{
             ...scale(-1 / 5),
-            display: `block`,
             marginLeft: rhythm(1 / 8),
-            marginBottom: rhythm(1),
+            marginTop: rhythm(1),
+            marginBottom: 0,
             opacity: 0.8,
+            fontWeight: 'bold',
+            fontFamily: 'sans-serif',
           }}
         >
-          <PublicationInfo
-            date={post.frontmatter.date}
-            formattedDate={post.frontmatter.formattedDate}
-            timeToRead={post.timeToRead}
-          />
+          DRAFT
         </p>
-
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
+      ) : null}
+      <h1
+        style={{
+          marginTop: post.frontmatter.published ? rhythm(1) : 0,
+          marginBottom: 0,
+        }}
+      >
+        {post.frontmatter.title}
+      </h1>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginLeft: rhythm(1 / 8),
+          marginBottom: rhythm(1),
+          opacity: 0.8,
+        }}
+      >
+        <PublicationInfo
+          date={post.frontmatter.date}
+          formattedDate={post.frontmatter.formattedDate}
+          timeToRead={post.timeToRead}
         />
-        <Bio />
+      </p>
 
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio />
+
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
+  )
 }
-
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
