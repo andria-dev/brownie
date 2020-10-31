b2dad54f6b1c07b2b2b5ad280d8e32ca12ae3432
ChrisBrownie55 <chbphone55@gmail.com>
2020-10-31 11:18:45 -0500

    Only show published articles in next and previous buttons.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,66 +2,76 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+	const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post/index.js`)
-  return graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                published
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+	const blogPost = path.resolve(`./src/templates/blog-post/index.js`)
+	return graphql(
+		`
+			{
+				allMarkdownRemark(
+					sort: { fields: [frontmatter___date], order: DESC }
+					limit: 1000
+				) {
+					edges {
+						node {
+							fields {
+								slug
+							}
+							frontmatter {
+								title
+								published
+							}
+						}
+					}
+				}
+			}
+		`,
+	).then((result) => {
+		if (result.errors) {
+			throw result.errors
+		}
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+		// Create blog posts pages.
+		const posts = result.data.allMarkdownRemark.edges.map((post) => post.node)
+		const ifPublished = (post) => post.frontmatter.published
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+		posts.forEach((post, index) => {
+			const previous =
+				index === posts.length - 1
+					? null
+					: posts.slice(index + 1).find(ifPublished)
+			const next =
+				index === 0
+					? null
+					: posts
+							.slice(0, index - 1)
+							.reverse()
+							.find(ifPublished)
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next
-        }
-      })
-    })
+			createPage({
+				path: post.fields.slug,
+				component: blogPost,
+				context: {
+					slug: post.fields.slug,
+					previous,
+					next,
+				},
+			})
+		})
 
-    return null
-  })
+		return null
+	})
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+	const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value
-    })
-  }
+	if (node.internal.type === `MarkdownRemark`) {
+		const value = createFilePath({ node, getNode })
+		createNodeField({
+			name: `slug`,
+			node,
+			value,
+		})
+	}
 }
