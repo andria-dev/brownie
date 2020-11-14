8dd1a6173f515e4477affa5413c7ec3c949655ff
ChrisBrownie55 <chbphone55@gmail.com>
2020-11-14 11:13:03 -0600

    Add favicon and remove caching for dev

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,35 @@ export default class BlogDocument extends Document {
 			<Html lang="en-US">
 				<Head>
 					<meta charSet="UTF-8" />
+
+					{/* Favicon info */}
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/favicons/apple-touch-icon.png?V=PYAmmRp8gy"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicons/favicon-32x32.png?V=PYAmmRp8gy"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicons/favicon-16x16.png?V=PYAmmRp8gy"
+					/>
+					<link rel="manifest" href="/favicons/site.webmanifest?V=PYAmmRp8gy" />
+					<link
+						rel="mask-icon"
+						href="/favicons/safari-pinned-tab.svg?V=PYAmmRp8gy"
+						color="#668bfe"
+					/>
+					<link rel="shortcut icon" href="/favicons/favicon.ico?V=PYAmmRp8gy" />
+					<meta name="msapplication-TileColor" content="#ffc40d" />
+					<meta name="theme-color" content="#fafafa" />
+
 					{this.props.styleTags}
 					<TypographyStyle typography={typography} />
 					<GoogleFont typography={typography} />
