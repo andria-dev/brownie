ea215bea31127bb4128eb67e3b8e640cdea19452
Andria Brown <andria_girl@pm.me>
2023-01-05 03:38:43 -0500

    migrating to new versions

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,8 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
-import {ServerStyleSheet} from 'styled-components'
-import flush from 'styled-jsx/server'
 import {TypographyStyle, GoogleFont} from 'react-typography'
 import typography from '../utils/typography'
 
 export default class BlogDocument extends Document {
-	static getInitialProps({renderPage}) {
-		// Step 1: Create an instance of ServerStyleSheet
-		const sheet = new ServerStyleSheet()
-
-		// Step 2: Retrieve styles from components in the page
-		const page = renderPage((App) => (props) =>
-			sheet.collectStyles(<App {...props} />),
-		)
-
-		// Step 3: Extract the styles as <style> tags
-		const styleTags = sheet.getStyleElement()
-		const styles = flush()
-
-		// Step 4: Pass styleTags as a prop
-		return {...page, styleTags, styles}
-	}
-
 	render() {
 		const shouldRenderScripts =
 			this.props.unstable_runtimeJS || process.env.NODE_ENV !== 'production'
